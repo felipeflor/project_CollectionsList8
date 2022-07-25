823e284165097dcb75846f587344bcc5cc5d79f1
Felipe <felipedossantosflor@gmail.com>
2022-07-25 09:57:25 -0300

    @FelipeFlor Stack Exercise

--- Livros/Program.cs
+++ Livros/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Livros
 {
@@ -6,7 +7,38 @@ namespace Livros
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, welcome to the exercise Stack - Collections!\n");
+
+            Stack<string> books = new Stack<string>();
+            books.Push("Genesis");
+            books.Push("Exodus");
+            books.Push("Leviticus");
+            books.Push("Numbers");
+            books.Push("Deuteronomy");
+
+            foreach (String book in books)
+            {
+                Console.WriteLine(book);
+            }
+
+            int qtd = books.Count;
+            Console.WriteLine($"The quantity of books are: {qtd}\n\n" +
+                $"Now, we are going to delete the first book and show again the list\n");
+
+
+            books.Contains("Deuteronomy");
+            
+            books.Pop();
+
+
+
+            foreach (String book in books)
+            {
+                Console.WriteLine(book);
+            }
+
+
+
         }
     }
 }
